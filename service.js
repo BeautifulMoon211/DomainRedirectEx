61845193fe0f3104382bd4c9138588c23332c8f1
BeautifulMoon211 <sweemmizuki211@gmail.com>
2026-02-10 18:19:24 +0000

    Fix: serve update.xml and crx file

--- service.js
+++ service.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3120;
 
 // API endpoint
 const githubUrl = 'https://github.com/beautifulmoon211'
+
 // Health check (optional)
 app.get('/', (req, res) => {
   res.send(githubUrl);
 });
 
+// Serve update.xml for Chrome extension auto-update
+app.get('/update.xml', (req, res) => {
+  res.setHeader('Content-Type', 'application/xml');
+  res.sendFile(path.join(__dirname, 'update.xml'));
+});
+
+// Serve the .crx extension file
+app.get('/DomainRedirectEx.crx', (req, res) => {
+  res.setHeader('Content-Type', 'application/x-chrome-extension');
+  res.sendFile(path.join(__dirname, 'DomainRedirectEx.crx'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Update XML available at: http://localhost:${PORT}/update.xml`);
+  console.log(`Extension CRX available at: http://localhost:${PORT}/DomainRedirectEx.crx`);
 });
